614baee2a6c5337dba3c3dfa0d65cf96abc350e5
profounddark <39137591+profounddark@users.noreply.github.com>
2020-06-30 22:55:11 -0700

    added dark/light modes, CSS vars

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import Footer from './footer.js';
 import Navbar from './navbar.js';
+import styles from './layout.module.css';
 
-const style = {
-    mainContainer: {
-        position: "relative",
-        minHeight: "100vh"
-    },
-    contentWrap: {
-        paddingBottom: "2.5rem",
-        maxWidth: "600px",
-        margin: "1rem auto 0 auto"
-    }
-}
 
 function Layout(props) {
     return (
-        <div style={style.mainContainer}>
+        <div className={styles.mainContainer}>
             <Navbar />
 
-            <div style={style.contentWrap}>
+            <div className={styles.contentWrap}>
                 {props.children}
             </div>
             <Footer />
